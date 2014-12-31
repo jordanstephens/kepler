e1bee5b61d6cac001dbabd5c26921b0097c4086a
Jordan Stephens <iam@jordanstephens.net>
2014-12-30 21:45:59 -0500

    using rounded values for specs

--- spec/kepler/orbit_spec.rb
+++ spec/kepler/orbit_spec.rb
@@ -10,19 +10,19 @@ describe Kepler::Orbit do
       @orbit = Kepler::Orbit.new(r, v)
     end
 
-    it { expect(@orbit.angular_momentum).to eql(Vector[-25385.17, 6669.484999999999, -52070.740000000005]) }
-    it { expect(@orbit.radial_velocity).to eql(0.5574679274498466) }
-    it { expect(@orbit.eccentricity).to eql(Vector[-0.09160485604616679, -0.14220737156769422, 0.02644392824064549]) }
-    it { expect(@orbit.semimajor_axis).to eql(8788.095117377656) }
-    it { expect(@orbit.semilatus_rectum).to eql(8530.483818970712) }
-    it { expect(@orbit.inclination).to eql(153.2492285182475) }
-    it { expect(@orbit.node_line).to eql(Vector[-6669.484999999999, -25385.17, 0.0]) }
-    it { expect(@orbit.right_ascension).to eql(255.27928533439615) }
-    it { expect(@orbit.argument_of_periapsis).to eql(20.068316650582467) }
-    it { expect(@orbit.true_anomaly).to eql(28.445628306614967) }
-    it { expect(@orbit.apoapsis).to eql(10292.725501794834) }
-    it { expect(@orbit.periapsis).to eql(7283.464732960478) }
-    it { expect(@orbit.period).to eql(8198.857616829207) }
+    it { expect(@orbit.angular_momentum.round(5)).to eql(Vector[-25385.17, 6669.485, -52070.74]) }
+    it { expect(@orbit.radial_velocity.round(5)).to eql(0.55747) }
+    it { expect(@orbit.eccentricity.round(5)).to eql(Vector[-0.0916, -0.14221, 0.02644]) }
+    it { expect(@orbit.semimajor_axis.round(5)).to eql(8788.09512) }
+    it { expect(@orbit.semilatus_rectum.round(5)).to eql(8530.48382) }
+    it { expect(@orbit.inclination.round(5)).to eql(153.24923) }
+    it { expect(@orbit.node_line.round(5)).to eql(Vector[-6669.485, -25385.17, 0.0]) }
+    it { expect(@orbit.right_ascension.round(5)).to eql(255.27929) }
+    it { expect(@orbit.argument_of_periapsis.round(5)).to eql(20.06832) }
+    it { expect(@orbit.true_anomaly.round(5)).to eql(28.44563) }
+    it { expect(@orbit.apoapsis.round(5)).to eql(10292.7255) }
+    it { expect(@orbit.periapsis.round(5)).to eql(7283.46473) }
+    it { expect(@orbit.period.round(5)).to eql(8198.85762) }
   end
 
   context "when updating r and v after dt" do
@@ -33,11 +33,11 @@ describe Kepler::Orbit do
       @orbit = Kepler::Orbit.new(r, v)
     end
 
-    it { expect(@orbit.universal_anomaly(3600)).to eql(253.53449076412872) }
+    it { expect(@orbit.universal_anomaly(3600).round(5)).to eql(253.53449) }
     it do
       @orbit.update!(3600)
-      expect(@orbit.r).to eql(Vector[-3297.7686251992823, 7413.396645787405, 0.0])
-      expect(@orbit.v).to eql(Vector[-8.297603024266525, -0.9640449446737689, -0.0])
+      expect(@orbit.r.round(5)).to eql(Vector[-3297.76863, 7413.39665, 0.0])
+      expect(@orbit.v.round(5)).to eql(Vector[-8.2976, -0.96404, -0.0])
     end
   end
 
@@ -55,8 +55,8 @@ describe Kepler::Orbit do
     end
 
     it do
-      expect(@orbit.r).to eql(Vector[-4039.895923201738, 4814.560480182377, 3628.6247021718837])
-      expect(@orbit.v).to eql(Vector[-10.385987618194685, -4.771921637340853, 1.7438750000000005])
+      expect(@orbit.r.round(5)).to eql(Vector[-4039.89592, 4814.56048, 3628.6247])
+      expect(@orbit.v.round(5)).to eql(Vector[-10.38599, -4.77192, 1.74388])
     end
   end
 
@@ -73,8 +73,8 @@ describe Kepler::Orbit do
     end
 
     it do
-      expect(@orbit.r).to eql(Vector[1311.5636463724593, 4699.598648496937, 4701.881415410206])
-      expect(@orbit.v).to eql(Vector[-5.641883624845517, 4.379639136504691, -2.803740788571159])
+      expect(@orbit.r.round(5)).to eql(Vector[1311.56365, 4699.59865, 4701.88142])
+      expect(@orbit.v.round(5)).to eql(Vector[-5.64188, 4.37964, -2.80374])
     end
   end
 
@@ -86,7 +86,7 @@ describe Kepler::Orbit do
       theta = 30
       r_p = Kepler::Orbit.perifocal_position(h, e, theta)
 
-      expect(r_p).to eql(Vector[6284.962345761189, 3628.6247021718837, 0.0])
+      expect(r_p.round(5)).to eql(Vector[6284.96235, 3628.6247, 0.0])
     end
   end
 
@@ -98,7 +98,7 @@ describe Kepler::Orbit do
       theta = 30
       v_p = Kepler::Orbit.perifocal_velocity(h, e, theta)
 
-      expect(v_p).to eql(Vector[-2.4912499999999995, 11.290471574355966, 0.0])
+      expect(v_p.round(5)).to eql(Vector[-2.49125, 11.29047, 0.0])
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 require "rspec"
 require "pry-byebug"
 require "kepler"
+
+# adding a #round method to Vector so we can set expectations on float components
+class Vector
+  def round(ndigits = 0)
+    map { |e| e.round(ndigits) }
+  end
+end
